c3f78e86c4749a483c2f22a71fd0ed1d63497df9
EKayatz <eliaskayatz04@gmail.com>
2024-02-22 07:17:15 +0100

    client

---
+++ client/src/App.tsx
@@ -0,0 +1,185 @@
+import AccountCircleOutlined from "@mui/icons-material/AccountCircleOutlined";
+import ChatBubbleOutline from "@mui/icons-material/ChatBubbleOutline";
+import PeopleAltOutlined from "@mui/icons-material/PeopleAltOutlined";
+import StarOutlineRounded from "@mui/icons-material/StarOutlineRounded";
+import VillaOutlined from "@mui/icons-material/VillaOutlined";
+import CssBaseline from "@mui/material/CssBaseline";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import {
+  GitHubBanner,
+  LegacyAuthProvider as AuthProvider,
+  Refine,
+  AuthBindings,
+} from "@refinedev/core";
+import {
+  ErrorComponent,
+  useNotificationProvider,
+  ReadyPage,
+  RefineSnackbarProvider,
+} from "@refinedev/mui";
+
+import routerProvider from "@refinedev/react-router-v6/legacy";
+import dataProvider from "@refinedev/simple-rest";
+import axios from "axios";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+import { CredentialResponse } from "./interfaces/google";
+import { parseJwt } from "./utils/parse-jwt";
+
+import {
+  AgentProfile,
+  Agents,
+  AllProperties,
+  CreateProperty,
+  EditProperty,
+  Home,
+  Login,
+  MyProfile,
+  PropertyDetails,
+} from "./pages";
+import { Title, Sider, Layout, Header } from "./components/layout";
+
+const axiosInstance = axios.create();
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (config.headers) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+function App() {
+  const authProvider: AuthBindings = {
+    login: async ({ credential }: CredentialResponse) => {
+      const profileObj = credential ? parseJwt(credential) : null;
+
+      if (profileObj) {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
+            ...profileObj,
+            avatar: profileObj.picture,
+          })
+        );
+
+        localStorage.setItem("token", `${credential}`);
+
+        return {
+          success: true,
+          redirectTo: "/",
+        };
+      }
+
+      return {
+        success: false,
+      };
+    },
+    logout: async () => {
+      const token = localStorage.getItem("token");
+
+      if (token && typeof window !== "undefined") {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        axios.defaults.headers.common = {};
+        window.google?.accounts.id.revoke(token, () => {
+          return {};
+        });
+      }
+
+      return {
+        success: true,
+        redirectTo: "/login",
+      };
+    },
+    onError: async (error) => {
+      console.error(error);
+      return { error };
+    },
+    check: async () => {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        return {
+          authenticated: true,
+        };
+      }
+
+      return {
+        authenticated: false,
+        error: {
+          message: "Check failed",
+          name: "Token not found",
+        },
+        logout: true,
+        redirectTo: "/login",
+      };
+    },
+    getPermissions: async () => null,
+    getIdentity: async () => {
+      const user = localStorage.getItem("user");
+      if (user) {
+        return JSON.parse(user);
+      }
+
+      return null;
+    },
+  };
+
+  return (
+    <ColorModeContextProvider>
+      <GitHubBanner />
+      <CssBaseline />
+      <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+      <RefineSnackbarProvider>
+        <Refine
+          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          notificationProvider={useNotificationProvider}
+          ReadyPage={ReadyPage}
+          catchAll={<ErrorComponent />}
+          resources={[
+            {
+              name: "properties",
+              list: AllProperties,
+              show: PropertyDetails,
+              create: CreateProperty,
+              edit: EditProperty,
+              icon: <VillaOutlined />,
+            },
+            {
+              name: "agents",
+              list: Agents,
+              show: AgentProfile,
+              icon: <PeopleAltOutlined />,
+            },
+            {
+              name: "reviews",
+              list: Home,
+              icon: <StarOutlineRounded />,
+            },
+            {
+              name: "messages",
+              list: Home,
+              icon: <ChatBubbleOutline />,
+            },
+            {
+              name: "my-profile",
+              options: { label: "My Profile " },
+              list: MyProfile,
+              icon: <AccountCircleOutlined />,
+            },
+          ]}
+          Title={Title}
+          Sider={Sider}
+          Layout={Layout}
+          Header={Header}
+          legacyRouterProvider={routerProvider}
+          //legacyAuthProvider={authProvider}
+          LoginPage={Login}
+          DashboardPage={Home}
+        />
+      </RefineSnackbarProvider>
+    </ColorModeContextProvider>
+  );
+}
+
+export default App;
